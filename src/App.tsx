cabb4adc8cf25c2c27b43034956a6c061f7c4ce0
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2020-09-06 00:44:05 +0500

    structure is complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,37 @@
 import React from 'react';
 
+import { QuestionCard } from './components/QuestionCard'
 
 function App() {
+
+  const startQuiz = async() => {}
+  
+  const nextQuestion = async() => {}
+
+
+
   return (
     <div className="App">
-       QUIZ APP WITH TS PWA AND FCM 
+
+      <h1> Quiz App </h1>
+
+      <button className="start" onClick={startQuiz}>
+         Begin Quiz
+      </button>
+
+      <p className="score"> 
+         Score: 
+      </p>
+
+      <p>
+        Loading Question ......
+      </p>
+
+      <QuestionCard />
+
+      <button className="next" onClick={nextQuestion}> 
+         Next Question
+      </button>
     </div>
   );
 }
---
+++ src/components/QuestionCard.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const QuestionCard = () => {
+    return (
+        <div>
+            Question Card
+        </div>
+    )
+}
+
+
