ff4ccddb095f679815c17272303a8b6d78667d0b
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2020-09-06 02:45:01 +0500

    quiz app part 1 complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,99 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { QuestionCard } from './components/QuestionCard'
+import { QuestionCard } from './components/QuestionCard';
+import { fetchQuestions, Difficulty, QuestionState } from './API'
+
+const TOTAL_QUESTIONS = 10;
+
+type AnswerObject = {
+  question: string;
+  answer: string;
+  correct: boolean;
+  correctAnswer: string;
+}
 
 function App() {
 
-  const startQuiz = async() => {}
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState<QuestionState[]>([]);
+  const [number, setNumber] = useState(0);
+  const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([]);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(true);
+
+  console.log(questions);
   
-  const nextQuestion = async() => {}
+
+  const startQuiz = async() => {
+    setLoading(true);
+    setGameOver(false);
+
+    const newQuestions = await fetchQuestions(TOTAL_QUESTIONS, Difficulty.EASY)
+
+    setQuestions(newQuestions);
+    setScore(0);
+    setUserAnswers([]);
+    setNumber(0);
+    setLoading(false);
 
 
+
+  }
+
+  const nextQuestion = async() => {
+    const nextQuestion = number + 1;
+
+    if(nextQuestion === TOTAL_QUESTIONS) {
+      setGameOver(true);
+    }
+
+    else {
+      setNumber(nextQuestion)
+    }    
+  }
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {}
 
   return (
     <div className="App">
 
       <h1> Quiz App </h1>
 
-      <button className="start" onClick={startQuiz}>
-         Begin Quiz
-      </button>
+      { gameOver || userAnswers.length === TOTAL_QUESTIONS ? (        
+        <button className="start" onClick={startQuiz}>
+           Begin Quiz
+        </button>  ) : null
+      } 
 
-      <p className="score"> 
-         Score: 
-      </p>
+      { !gameOver ? (     
+        <p className="score">
+          Score: {score}
+        </p> ) : null 
+      }
 
-      <p>
-        Loading Question ......
-      </p>
+      { loading ? (
+        <p>
+          Loading Question ......
+        </p> ) : null
+      } 
 
-      <QuestionCard />
+      { !loading && !gameOver ? (
+        <QuestionCard 
+          questionNum = { number + 1 }
+          totalQuestions = {TOTAL_QUESTIONS }
+          question = { questions[number].question }
+          answers = { questions[number].answers }
+          userAnswer={ userAnswers ? userAnswers[number]:  undefined }
+          callback={ checkAnswer }
+       /> ) : null
+      }
 
-      <button className="next" onClick={nextQuestion}> 
-         Next Question
-      </button>
+      { !gameOver && !loading && userAnswers.length === number + 1 && number !== TOTAL_QUESTIONS - 1 ? (
+        <button className="next" onClick={nextQuestion}>
+           Next 
+        </button> ) : null 
+      }
+
     </div>
   );
 }
--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,9 +1,33 @@
 import React from 'react'
 
-export const QuestionCard = () => {
+
+type Props = {
+    question: string,
+    answers: string[],
+    callback: any,
+    userAnswer: any,
+    questionNum: number,
+    totalQuestions: number,
+}
+
+export const QuestionCard: React.FC<Props> = ({question, answers, callback, userAnswer, questionNum, totalQuestions}) => {
     return (
         <div>
-            Question Card
+            <p>
+              Question: {questionNum} / {totalQuestions}       
+            </p>           
+
+            <p dangerouslySetInnerHTML={{ __html: question }} />  
+
+            <div>
+                {answers.map(answer => (
+                    <div>
+                        <button disabled={userAnswer} onClick={callback}>
+                            <span dangerouslySetInnerHTML={{ __html: answer }} />
+                        </button>
+                    </div>
+                ))}                
+            </div>            
         </div>
     )
 }
